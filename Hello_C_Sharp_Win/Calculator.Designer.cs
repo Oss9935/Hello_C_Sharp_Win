c005dccf8064a030beaa04a8b738c09d1e0b6af3
BBKIM <jotun9935@gmai.com>
2016-12-03 02:54:06 +0900

    version1

---
+++ Hello_C_Sharp_Win/Calculator.Designer.cs
@@ -0,0 +1,64 @@
+﻿namespace Hello_C_Sharp_Win
+{
+     partial class Calculator
+     {
+          /// <summary>
+          /// 필수 디자이너 변수입니다.
+          /// </summary>
+          private System.ComponentModel.IContainer components = null;
+
+          /// <summary>
+          /// 사용 중인 모든 리소스를 정리합니다.
+          /// </summary>
+          /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+          protected override void Dispose(bool disposing)
+          {
+               if (disposing && (components != null))
+               {
+                    components.Dispose();
+               }
+               base.Dispose(disposing);
+          }
+
+          #region Windows Form 디자이너에서 생성한 코드
+
+          /// <summary>
+          /// 디자이너 지원에 필요한 메서드입니다. 
+          /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+          /// </summary>
+          private void InitializeComponent()
+          {
+               this.HelloLable = new System.Windows.Forms.Label();
+               this.SuspendLayout();
+               // 
+               // HelloLable
+               // 
+               this.HelloLable.AutoSize = true;
+               this.HelloLable.Cursor = System.Windows.Forms.Cursors.Hand;
+               this.HelloLable.Location = new System.Drawing.Point(25, 31);
+               this.HelloLable.Name = "HelloLable";
+               this.HelloLable.Size = new System.Drawing.Size(105, 12);
+               this.HelloLable.TabIndex = 0;
+               this.HelloLable.Text = "여기를 클락하세요";
+               this.HelloLable.CursorChanged += new System.EventHandler(this.HelloLable_Click);
+               this.HelloLable.Click += new System.EventHandler(this.HelloLable_Click);
+               // 
+               // Calculator
+               // 
+               this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+               this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+               this.ClientSize = new System.Drawing.Size(374, 365);
+               this.Controls.Add(this.HelloLable);
+               this.Name = "Calculator";
+               this.Text = "Form1";
+               this.ResumeLayout(false);
+               this.PerformLayout();
+
+          }
+
+          #endregion
+
+          private System.Windows.Forms.Label HelloLable;
+     }
+}
+
---
+++ Hello_C_Sharp_Win/Calculator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hello_C_Sharp_Win
+{
+     public partial class Calculator : Form
+     {
+          public Calculator()
+          {
+               InitializeComponent();
+          }
+
+          private void HelloLable_Click(object sender, EventArgs e)
+          {
+               HelloLable.Text = "Hello C#";
+
+               int number1 = 1;
+               int number2 = 2;
+
+               int sum = number1 + number2;
+
+               HelloLable.Text = sum.ToString();
+
+
+          }
+     }
+}
