8bb7d51f58f5cbf0b0986eb525c88ea3562ac77e
BBKIM <jotun9935@gmai.com>
2016-12-03 12:38:49 +0900

    version2 : simple add

--- Hello_C_Sharp_Win/Calculator.Designer.cs
+++ Hello_C_Sharp_Win/Calculator.Designer.cs
@@ -29,6 +29,10 @@
           private void InitializeComponent()
           {
                this.HelloLable = new System.Windows.Forms.Label();
+               this.Input1 = new System.Windows.Forms.TextBox();
+               this.Input2 = new System.Windows.Forms.TextBox();
+               this.getResultButton = new System.Windows.Forms.Button();
+               this.ResultOutput = new System.Windows.Forms.TextBox();
                this.SuspendLayout();
                // 
                // HelloLable
@@ -43,14 +47,50 @@
                this.HelloLable.CursorChanged += new System.EventHandler(this.HelloLable_Click);
                this.HelloLable.Click += new System.EventHandler(this.HelloLable_Click);
                // 
+               // Input1
+               // 
+               this.Input1.Location = new System.Drawing.Point(27, 85);
+               this.Input1.Name = "Input1";
+               this.Input1.Size = new System.Drawing.Size(65, 21);
+               this.Input1.TabIndex = 1;
+               // 
+               // Input2
+               // 
+               this.Input2.Location = new System.Drawing.Point(98, 85);
+               this.Input2.Name = "Input2";
+               this.Input2.Size = new System.Drawing.Size(65, 21);
+               this.Input2.TabIndex = 2;
+               // 
+               // getResultButton
+               // 
+               this.getResultButton.Location = new System.Drawing.Point(169, 83);
+               this.getResultButton.Name = "getResultButton";
+               this.getResultButton.Size = new System.Drawing.Size(36, 23);
+               this.getResultButton.TabIndex = 3;
+               this.getResultButton.Text = "=";
+               this.getResultButton.UseVisualStyleBackColor = true;
+               this.getResultButton.Click += new System.EventHandler(this.getResultButton_Click);
+               // 
+               // ResultOutput
+               // 
+               this.ResultOutput.Location = new System.Drawing.Point(211, 85);
+               this.ResultOutput.Name = "ResultOutput";
+               this.ResultOutput.Size = new System.Drawing.Size(100, 21);
+               this.ResultOutput.TabIndex = 4;
+               // 
                // Calculator
                // 
                this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
                this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
                this.ClientSize = new System.Drawing.Size(374, 365);
+               this.Controls.Add(this.ResultOutput);
+               this.Controls.Add(this.getResultButton);
+               this.Controls.Add(this.Input2);
+               this.Controls.Add(this.Input1);
                this.Controls.Add(this.HelloLable);
                this.Name = "Calculator";
                this.Text = "Form1";
+               this.Load += new System.EventHandler(this.Calculator_Load);
                this.ResumeLayout(false);
                this.PerformLayout();
 
@@ -59,6 +99,10 @@
           #endregion
 
           private System.Windows.Forms.Label HelloLable;
+          private System.Windows.Forms.TextBox Input1;
+          private System.Windows.Forms.TextBox Input2;
+          private System.Windows.Forms.Button getResultButton;
+          private System.Windows.Forms.TextBox ResultOutput;
      }
 }
 
--- Hello_C_Sharp_Win/Calculator.cs
+++ Hello_C_Sharp_Win/Calculator.cs
@@ -8,15 +8,34 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+
+
+/// <summary>
+/// C# 계산기 프로그램
+/// </summary>
 namespace Hello_C_Sharp_Win
 {
+
+     /// <summary>
+     /// 
+     /// </summary>
      public partial class Calculator : Form
      {
+
+          /// <summary>
+          /// 
+          /// </summary>
           public Calculator()
           {
                InitializeComponent();
           }
 
+          /// <summary>
+          /// lable1 클릭 시 발생하는 이벤트 핸들러
+          /// 
+          /// </summary>
+          /// <param name="sender"></param>
+          /// <param name="e"></param>
           private void HelloLable_Click(object sender, EventArgs e)
           {
                HelloLable.Text = "Hello C#";
@@ -29,6 +48,32 @@ namespace Hello_C_Sharp_Win
                HelloLable.Text = sum.ToString();
 
 
+          }
+
+
+          /// <summary>
+          /// 
+          /// </summary>
+          /// <param name="sender"></param>
+          /// <param name="e"></param>
+          private void Calculator_Load(object sender, EventArgs e)
+          {
+
+          }
+
+          /// <summary>
+          /// Result : get sum of two event args
+          /// </summary>
+          /// <param name="sender"></param>
+          /// <param name="e"></param>
+          private void getResultButton_Click(object sender, EventArgs e)
+          {
+               int number1 = Convert.ToInt32(Input1.Text);
+               int number2 = Convert.ToInt32(Input2.Text);
+
+               int sum = number1 + number2;
+
+               ResultOutput.Text = sum.ToString();
           }
      }
 }
